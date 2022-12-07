d9b6ab523e7936227e5a94260da6c029ddc74b63
Kamil Piróg <62013599+kpirog@users.noreply.github.com>
2022-12-07 12:06:14 +0100

    Add arrows count limit

--- Assets/Scripts/Player/PlayerShootingController.cs
+++ Assets/Scripts/Player/PlayerShootingController.cs
@@ -1,4 +1,3 @@
-using System;
 using Elympics;
 using Medicine;
 using Projectiles;
@@ -8,8 +7,14 @@ namespace Player
 {
     public class PlayerShootingController : ElympicsMonoBehaviour
     {
+        [SerializeField] private ElympicsInt pushArrowsCount;
+        [SerializeField] private ElympicsInt iceArrowsCount;
+        [SerializeField] private ElympicsInt invertedArrowsCount;
         [Inject] private PlayerAnimationHandler AnimationHandler { get; }
 
+        private ElympicsInt _arrowsCount = new();
+        
+        private ArrowType _currentArrowType;
         private string _arrowPrefabPath;
         private float _drawingForce;
         private bool CanRelease => _drawingForce > 0f;
@@ -26,18 +31,25 @@ namespace Player
                 default:
                 case ArrowType.Push:
                     _arrowPrefabPath = "Prefabs/Projectiles/Push Arrow";
+                    _arrowsCount = pushArrowsCount;
                     break;
                 case ArrowType.Ice:
                     _arrowPrefabPath = "Prefabs/Projectiles/Ice Arrow";
+                    _arrowsCount = iceArrowsCount;
                     break;
                 case ArrowType.Inverted:
                     _arrowPrefabPath = "Prefabs/Projectiles/Inverted Arrow";
+                    _arrowsCount = invertedArrowsCount;
                     break;
             }
+
+            _currentArrowType = type;
         }
 
         public void ProcessShoot(bool input, Vector2 mousePosition)
         {
+            if (_arrowsCount.Value == 0) return;
+            
             if (input)
             {
                 DrawBow();
@@ -64,11 +76,31 @@ namespace Player
         private void CreateArrow(Vector2 mousePosition)
         {
             var arrow = ElympicsInstantiate(_arrowPrefabPath, ElympicsPlayer.All).GetComponent<Arrow>();
-
             arrow.Setup((mousePosition - (Vector2)transform.position).normalized, transform.position, _drawingForce,
                 ElympicsBehaviour);
 
+            DecreaseArrowsCount();
+
             ResetBow();
+        }
+
+        private void DecreaseArrowsCount()
+        {
+            _arrowsCount.Value--;
+
+            switch (_currentArrowType)
+            {
+                default:
+                case ArrowType.Push:
+                    pushArrowsCount.Value = _arrowsCount;
+                    break;
+                case ArrowType.Ice:
+                    iceArrowsCount.Value = _arrowsCount;
+                    break;
+                case ArrowType.Inverted:
+                    invertedArrowsCount.Value = _arrowsCount;
+                    break;
+            }
         }
 
         private void ResetBow()
